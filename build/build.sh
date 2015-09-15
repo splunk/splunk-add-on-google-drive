279b462857cefd3b2e08f75185472cc87632f526
David Poncelow <dponcelow@splunk.com>
2015-09-15 14:35:08 -0700

    Fixed a couple small issues with build script

--- build/build.sh
+++ build/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-FILENAME_BASE=${PWD##*/}
 BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )
+FILENAME_BASE=${BASEDIR##*/}
 VERSION=`grep version ${BASEDIR}/default/app.conf | sed 's/.*=\s*\([1-9\.]*\)/\1/' | tr -d '[:space:]'`
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 if [ "$GIT_BRANCH" = "master" ]
 then
-    FILENAME="${FILENAME_BASE}-${VERSION}.tar.gz"
+    FILENAME="${FILENAME_BASE}-${VERSION}.spl"
 else
     CURRENT_COMMIT=$(git rev-parse --short HEAD)
     FILENAME="${FILENAME_BASE}-${GIT_BRANCH}-${CURRENT_COMMIT}-${VERSION}.spl"
